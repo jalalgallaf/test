ec85dd217e79366f5bfe35bc76de473bf69b139d
jalal <jalal2008z@gmail.com>
2023-07-31 01:53:08 +0300

    adding hello

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from "./hello";
+
+function App() {
+  return (
+    <div className="App">
+
+      <Hello name={"jalal"} />
+
+
+    </div>
+  );
+}
+
+export default App;
